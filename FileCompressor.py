9238a8b2cf64a33c10e432c78121774afdd0313a
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2023-05-05 19:28:38 -0700

    setup the argparser

--- FileCompressor.py
+++ FileCompressor.py
@@ -1,4 +1,6 @@
 from collections import Counter # this is for creating the frequency table
+import argparse
+import sys
 
 '''How this program is going to work
 User is going to run the program, with the only other arg being the text file they want to compress
@@ -9,4 +11,20 @@ The program is going to read in that text file, perform text compression using H
 Output will be a message that everything went good, and the user should see a new file in their current working directory. The new file is the compressed file
 '''
 
+# even though we are only using one arg, argparser makes sure the user uses it right and also prints out a usage message so that we don't have to, which is nice
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="input text file to be compressed", required=True)
+args = parser.parse_args()
 
+inputFile = args.input
+extension = inputFile.split(".")[1]
+
+if extension != "txt":
+    print("Please only pass in a text file")
+    sys.exit()
+
+'''TODO
+- make sure the filename they passed in exists in the cwd and try to read in the file 
+- save the text into a string
+- implement the actual algorithm 
+'''
