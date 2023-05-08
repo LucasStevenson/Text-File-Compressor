d05af32287e64b458da5d024f1184fd1d53bc69f
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2023-05-08 01:22:02 -0700

    separated logic into separate classes. also added more args for specifying decompress or compress

--- FileCompressor.py
+++ FileCompressor.py
@@ -27,50 +27,63 @@ class Node:
     def __str__(self):
         return repr(f"{self.char}: {self.freq}")
 
-def build_huffman_tree(text):
-    freq_table = Counter(text) # the frequency table that maps each character to how often it appears in the text
-    nodes = [ Node(char, freq) for char, freq in freq_table.items() ]
-    heapq.heapify(nodes) # convert `nodes` to a min-heap
-    while len(nodes) > 1:
-        # keep popping off the first two elements, joining them, and inserting back into the heap 
-        lNode = heapq.heappop(nodes)
-        rNode = heapq.heappop(nodes)
-        newNode = Node(None, lNode.freq+rNode.freq)
-        newNode.left = lNode
-        newNode.right = rNode
-        heapq.heappush(nodes, newNode)
-    return nodes[0]
+class Compressor:
+    def __init__(self, text):
+        self.text = text
 
-def create_huffman_codes(node, s="", d={}):
-    # returns a dictionary that maps each letter to its huffman code
-    # going left on the huffman tree represents a 0 bit
-    # going right on the huffman tree represents a 1 bit
-    if node.char != None:
-        d[node.char] = s
-        return
-    create_huffman_codes(node.left, s+"0")
-    create_huffman_codes(node.right, s+"1")
-    return d
+    def __build_huffman_tree(self):
+        freq_table = Counter(self.text) # the frequency table that maps each character to how often it appears in the text
+        nodes = [ Node(char, freq) for char, freq in freq_table.items() ]
+        heapq.heapify(nodes) # convert `nodes` to a min-heap
+        while len(nodes) > 1:
+            # keep popping off the first two elements, joining them, and inserting back into the heap 
+            lNode = heapq.heappop(nodes)
+            rNode = heapq.heappop(nodes)
+            newNode = Node(None, lNode.freq+rNode.freq)
+            newNode.left = lNode
+            newNode.right = rNode
+            heapq.heappush(nodes, newNode)
+        return nodes[0]
 
-def compress(text):
-    root = build_huffman_tree(text) 
-    codes = create_huffman_codes(root)  
-    # convert text to compressed binary data
-    bits = "".join([ codes[c] for c in text ])
-    compressed_data = bytearray([int(bits[i:i+8], 2) for i in range(0, len(bits), 8)])
-    with open("compressed.bin", "wb") as f:
-        # in the compressed binary, we need to store the compressed data alongside the huffman codes dictionary
-        # this is so that we can actually decompress the file
-        pickle.dump(codes, f)
-        f.write(compressed_data)
-    print("Compression complete. Output written to 'compressed.bin'")
+    def __create_huffman_codes(self, node, s="", d={}):
+        # returns a dictionary that maps each letter to its huffman code
+        # going left on the huffman tree represents a 0 bit
+        # going right on the huffman tree represents a 1 bit
+        if node.char != None:
+            d[node.char] = s
+            return
+        self.__create_huffman_codes(node.left, s+"0")
+        self.__create_huffman_codes(node.right, s+"1")
+        return d
 
+    def compress(self):
+        root = self.__build_huffman_tree() 
+        codes = self.__create_huffman_codes(root)  
+        # convert text to compressed binary data
+        bits = "".join([ codes[c] for c in self.text ])
+        compressed_data = bytearray([int(bits[i:i+8], 2) for i in range(0, len(bits), 8)])
+        with open("compressed.bin", "wb") as f:
+            # in the compressed binary, we need to store the compressed data alongside the huffman codes dictionary
+            # this is so that we can actually decompress the file
+            pickle.dump(codes, f)
+            f.write(compressed_data)
+        print("Compression complete. Output written to 'compressed.bin'")
+
+
+class Decompressor:
+    def __init__(self, codes, compressed_data):
+        self.codes = codes
+        self.compressed_data = compressed_data
+
+    def decompress(self):
+        # TODO: complete this 
+        pass
 
 def main():
-    # even though we are only using one arg, argparser makes sure the user uses it right and also prints out a usage message so that we don't have to, which is nice
-    # also might add more arguments, for specifying whether to decompress or compress the input file
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="input text file to be compressed", required=True)
+    parser.add_argument("-c", "--compress", help="compress file option", action="store_true")
+    parser.add_argument("-d", "--decompress", help="decompress file option", action="store_true")
     args = parser.parse_args()
 
     filename = args.input
@@ -78,16 +91,33 @@ def main():
     if not os.path.exists(filename):
         print(f"'{filename}' does not exist")
         sys.exit()
-    # check that the file is a txt file
-    if not filename.endswith(".txt"): 
-        print("Please only pass in a text file and make sure the extension is '.txt'")
+    # make sure that the user either specified whether to compress or decompress the input file
+    if not (args.compress ^ args.decompress):
+        print("Must specify whether you are compressing or decopmressing this file using the '--compress' option or '--decompress' option")
         sys.exit()
 
-    # read the text in the file and save to a variable
-    with open(filename) as f:
-        text = f.read()
-        assert len(text) > 0, "Please provide a text file with text in it"
-    compress(text)
+    if args.compress: # we are compressing a file
+        # check that the file is a txt file
+        if not filename.endswith(".txt"): 
+            print("Please only pass in a text file and make sure the extension is '.txt'")
+            sys.exit()
+        # read the text in the file and save to a variable
+        with open(filename) as f:
+            text = f.read()
+            assert len(text) > 0, "Please provide a text file with text in it"
+        # compress the text
+        Compressor(text).compress()
+    elif args.decompress: # we are decompressing a file
+        # check that the file is a bin file
+        if not filename.endswith(".bin") or len(filename) < 5:
+            print("Please only pass in a binary file and make sure that the extension is '.bin'")
+            sys.exit()
+        with open(filename, "rb") as f:
+            huffman_codes = pickle.load(f)
+            compressed_data = f.read()
+            assert len(compressed_data) > 0, "Please provide a binary file with data in it"
+        # decompress the data
+        Decompressor(huffman_codes, compressed_data).decompress()
 
 if __name__ == "__main__":
     main()
